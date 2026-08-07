9cd952bbd31746247f19c7867ea328dbf65edd93
Animadik Singh <animadiksingh@gmail.com>
2026-08-07 23:32:26 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (69.88%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -5,7 +5,7 @@ public:
         if(n==1) return nums[0];
         if(nums[0]!=nums[1]) return nums[0];
         if(nums[n-1]!=nums[n-2]) return nums[n-1];
-        int low=0, high=n-2;
+        int low=1, high=n-2;
         while(low<=high){
             //Odd even concept
             //Example- [1,1,2,2,3,4,4,5,5]  --> {(1,1),(2,2),(3),(4,4),(5,5)} --> index->{(even,odd),(even,odd),(even),(odd,even),(odd,even)} 
