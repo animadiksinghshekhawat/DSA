2c9fc955b05707dc86ea82f86454bc72ba872ca4
Animadik Singh <animadiksingh@gmail.com>
2026-08-07 21:48:55 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (69.88%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+        int low=0, high=n-2;
+        while(low<=high){
+            //Odd even concept
+            //Example- [1,1,2,2,3,4,4,5,5]  --> {(1,1),(2,2),(3),(4,4),(5,5)} --> index->{(even,odd),(even,odd),(even),(odd,even),(odd,even)} 
+            int mid=(low+high)/2;
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]) return nums[mid];
+            //If you are at left half and element is in right half(even,odd)
+            if(mid%2==1 && nums[mid]==nums[mid-1] || mid%2==0 && nums[mid]==nums[mid+1]){
+                low=mid+1;
+            } 
+             //If you are at right half and element is in left half(odd.even)
+            else{
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+};
