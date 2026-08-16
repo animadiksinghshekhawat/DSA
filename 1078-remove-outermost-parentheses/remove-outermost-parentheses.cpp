86596962989fd51dc6eb2d6286e109f51c1f2f57
Animadik Singh <animadiksingh@gmail.com>
2026-08-16 14:27:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (97.89%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans="";
+        int level=0;
+        for(char ch :s ){
+            if (ch=='('){
+                if (level>0) ans+=ch;
+            
+            level++;
+            }
+            if(ch==')'){
+                level--;
+                if(level>0) ans+=ch;
+            }
+        }
+        return ans;
+    }
+};
