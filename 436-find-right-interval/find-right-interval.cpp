079076ee568d9146d66194d5ace60b9f243178f0
Animadik Singh <animadiksingh@gmail.com>
2026-09-05 21:17:25 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (69.49%) - LeetSync

--- 436-find-right-interval/find-right-interval.cpp
+++ 436-find-right-interval/find-right-interval.cpp
@@ -10,13 +10,21 @@ public:
         sort(starts.begin(), starts.end());
         vector<int> ans(n,-1);
         for (int i = 0; i < n; i++) {
-            int end = intervals[i][1];
-            auto it = lower_bound(starts.begin(), starts.end(),
-                                   make_pair(end, -1));
-
-            if (it != starts.end()) {
-                ans[i] = it->second;
+            int end=intervals[i][1];
+            int low=0;
+            int high= n-1;
+            int index=-1;
+            while(low<=high){
+                int mid=(low+high)/2;
+                if(starts[mid].first>=end){
+                    index=starts[mid].second;
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
             }
+            ans[i]=index;
         }
 
         return ans;
