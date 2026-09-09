26b0a51a4f7efe5bb80b3bc7462397df8c0cb6ce
Animadik Singh <animadiksingh@gmail.com>
2026-09-09 23:05:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (19.20%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+      long long ans = 0;
+
+        if (n >= 1000) {
+            ans += min(n, 999999LL) - 1000 + 1;
+        }
+
+        if (n >= 1000000) {
+            ans += (min(n, 999999999LL) - 1000000 + 1) * 2;
+        }
+
+        if (n >= 1000000000) {
+            ans += (min(n, 999999999999LL) - 1000000000 + 1) * 3;
+        }
+
+        if (n >= 1000000000000LL) {
+            ans += (min(n, 999999999999999LL) - 1000000000000LL + 1) * 4;
+        }
+
+        if (n >= 1000000000000000LL) {
+            ans += (n - 1000000000000000LL + 1) * 5;
+        }
+
+        return ans;
+    }
+};
