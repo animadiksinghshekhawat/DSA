f5d414516fc84f0cc9d3dada6e045b1e8dfad14a
Animadik Singh <animadiksingh@gmail.com>
2026-09-15 22:14:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.36%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1.0;
+        long long temp=n;
+        if(temp<0) temp=temp*-1;
+        while(temp){
+            if(temp%2){
+                ans=ans*x;
+                temp=temp-1;
+            }
+            else{
+                x=x*x;
+                temp=temp/2;
+            }
+        }
+        if(n<0) ans=1/ans;
+        return ans;
+    }
+};
